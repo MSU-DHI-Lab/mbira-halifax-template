51ec5990183c2f732ab0999bb8b62f73ec5af089
Matt Chebowski <matt.chebowski@peach.matrix.msu.edu>
2016-08-29 17:20:07 -0400

    user can add a reply to a conversation

--- includes/modalParticipateInConversation.php
+++ includes/modalParticipateInConversation.php
@@ -6,10 +6,22 @@
 		<div class='modal-x-btn'>
 			<img src='assets/svgs/close.svg' class='closeModalParticipateInConversation'>
 		</div>
-		<div class="modalContent">
+<!-- 		<div class="modalContent">
 			<textarea id="enterReply" class="textAdjust" name="enterReply" placeholder="Enter Your Reply Here" class="common" form="participateConversationForm"></textarea>
 		</div>
-		<a href="#" class="modalBottomButton">ADD REPLY</a>
+		<a href="#" class="modalBottomButton">ADD REPLY</a> -->
+
+		<div class="modalContent">
+			<form id="contributeForm" action="conversation-comment-post.php" method="post">
+				<textarea id="enterReply" class="textAdjust" name="comment" placeholder="Enter Your Reply Here" class="common"></textarea>
+				<input type="hidden" name="convo_id" value="<?php echo $convo ?>">
+				<input type="hidden" name="user" value="<?php echo $user->getId() ?>">
+				<input type="hidden" name="id" value="<?php echo $id ?>">
+				<input type="hidden" name="type" value="<?php echo $type ?>">
+				<a href="conversation-comment-post.php" onclick="this.parentNode.submit(); return false;" class="modalBottomButton">ADD REPLY</a>
+			</form>
+		</div>
+
 	</div>
 
 </div>
